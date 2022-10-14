d37f5e0509ef8f63791eaa111b884a825a57d3a4
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-10-14 11:43:42 -0400

    4chan scraper

--- index.js
+++ index.js
@@ -38,4 +38,50 @@ axios(url).then((response) => {
   console.log(thread);
 });
 
+const threads = [];
+let description = '';
+
+app.get('/', (req, res) => {
+  res.json('Welcome to the 4chan Webscraper API');
+});
+
+app.get('/biz', (req, res) => {
+  axios
+    .get('https://boards.4channel.org/biz')
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const item = $('div.board');
+
+      // $(item)
+      //   .find('div.post blockquote.postMessage')
+      //   .each(function () {
+      //     description = $(this).text();
+      //   });
+
+      $(item)
+        .find('span.subject')
+        .each(function () {
+          const title = $(this).text();
+
+          // console.log(title);
+          // const imgSrc = $(item)
+          //   .find('div.file a.fileThumb')
+          //   .children('img')
+          //   .eq(0)
+          //   .attr('src');
+          threads.push(title);
+        });
+      threads.map((thread) => {
+        if (thread.title === '') {
+          threads.pop(thread);
+        }
+      });
+      res.status(200).json({ threads });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
